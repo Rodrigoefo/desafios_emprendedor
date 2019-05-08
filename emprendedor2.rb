cb3fb9fc812536e7fc603c6732a61db17bac3bc0
Rodrigo <fuentesosorio@fuentesosorio.com>
2019-05-07 20:43:35 -0400

    Uploading emprendedor1 and emprendedor2

---
+++ emprendedor2.rb
@@ -0,0 +1,19 @@
+ingreso_de_datos= ARGV
+precio=ingreso_de_datos[0].to_f
+usuarios=ingreso_de_datos[1].to_f
+usuarioVP=ingreso_de_datos[2].to_f
+usuarioGR=ingreso_de_datos[3].to_f
+gastos=ingreso_de_datos[4].to_f
+
+
+utilidades = (precio*usuarios)
+utilidades= (utilidades+(usuarioVP*precio*2))-gastos
+
+if utilidades > 0
+    impuestos=utilidades*0.35
+    utilidades=utilidades*0.65
+    puts "Las utilidades estimadas (después de impuestos : #{impuestos}), son de #{utilidades} dolares  para el primer año"
+
+else
+  puts "Tu utilidades son negativas. Se estima que perderás #{utilidades} este año"
+end
